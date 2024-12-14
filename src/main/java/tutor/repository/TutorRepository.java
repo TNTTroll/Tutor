c6da3f433398b953aef434aaade756141125e869
TNTTroll <60446797+TNTTroll@users.noreply.github.com>
2024-12-14 12:02:47 +0300

    Update TutorRepository.java

--- src/main/java/tutor/repository/TutorRepository.java
+++ src/main/java/tutor/repository/TutorRepository.java
@@ -10,4 +10,11 @@ import reactor.core.publisher.Mono;
 public interface TutorRepository extends R2dbcRepository<TutorEntity, String> {
     Mono<Integer> countByLayerId(String layerId);
     Flux<TutorEntity> findAllByLayerId(String layerId);
+
+     @Query("""
+        UPDATE tutor
+        SET status = 'DELETED'
+        WHERE id = :id
+    """)
+    Mono<Void> deleteById(String id);
 }
