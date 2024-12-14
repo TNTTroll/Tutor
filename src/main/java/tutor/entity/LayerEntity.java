16b378f67132f1c9b0400d91ccfc64c3ac064457
TNTTroll <60446797+TNTTroll@users.noreply.github.com>
2024-12-14 12:00:59 +0300

    Update LayerEntity.java

--- src/main/java/tutor/entity/LayerEntity.java
+++ src/main/java/tutor/entity/LayerEntity.java
@@ -13,7 +13,7 @@ import org.springframework.data.relational.core.mapping.Table;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table("layers")
+@Table("layer")
 public class LayerEntity implements Persistable<String> {
     @Id
     @Column("id")
