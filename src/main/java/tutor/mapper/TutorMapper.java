3cf34207db7ea7266a8af7579ba6a09286450ae8
TNTTroll <60446797+TNTTroll@users.noreply.github.com>
2024-12-14 12:18:03 +0300

    Update TutorMapper.java

--- src/main/java/tutor/mapper/TutorMapper.java
+++ src/main/java/tutor/mapper/TutorMapper.java
@@ -3,27 +3,15 @@ package tutor.mapper;
 import tutor.dto.TutorDto;
 import tutor.entity.TutorEntity;
 import tutor.enums.TutorStatus;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface TutorMapper {
-    @Mapping(target = "status", source = "status", qualifiedByName = "getStringFromStatus")
+    @InheritInverseConfiguration
     TutorEntity toEntity(TutorDto dto);
 
-    @Mapping(target = "status", source = "status", qualifiedByName = "getStatusFromString")
     TutorDto toDto(TutorEntity entity);
-
-    @Named("getStringFromStatus")
-    private String getStringFromStatus(TutorStatus status) {
-        if (status == null) return TutorStatus.ACTIVE.getValue();
-        return status.getValue();
-    }
-
-    @Named("getStatusFromString")
-    private TutorStatus getStatusFromString(String status) {
-        if (status == null || status.isEmpty()) return TutorStatus.ACTIVE;
-        return TutorStatus.valueOf(status);
-    }
 }
