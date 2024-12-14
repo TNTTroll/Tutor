2cc42e1e92ff19bf096ef449d90e34ba27ecc597
TNTTroll <60446797+TNTTroll@users.noreply.github.com>
2024-12-14 12:05:27 +0300

    Delete src/main/java/tutor/service/UserToLayerService.java

--- src/main/java/tutor/service/UserToLayerService.java
+++
@@ -1,9 +0,0 @@
-package tutor.service;
-
-import main.java.tutor.entity.UserToLayerEntity;
-import reactor.core.publisher.Mono;
-
-public interface UserToLayerService {
-    Mono<UserToLayerEntity> getByUserId(String userId);
-    Mono<Boolean> checkMatchUserToLayer(String userId, String layerId);
-}
