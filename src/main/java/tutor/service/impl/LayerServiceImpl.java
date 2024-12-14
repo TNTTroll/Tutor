0d314bcc414de32b4a35c0cfbac7cf53a3cc9ea4
TNTTroll <60446797+TNTTroll@users.noreply.github.com>
2024-12-14 12:06:48 +0300

    Update LayerServiceImpl.java

--- src/main/java/tutor/service/impl/LayerServiceImpl.java
+++ src/main/java/tutor/service/impl/LayerServiceImpl.java
@@ -2,6 +2,7 @@ package tutor.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import tutor.entity.LayerEntity;
+import tutor.entity.TutorEntity;
 import tutor.repository.LayerRepository;
 import tutor.service.LayerService;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,19 @@ public class LayerServiceImpl {
     @Override
     public Mono<LayerEntity> getById(String id) {
         return layerRepository.findById(id);
+    }
+
+    @Override
+    public Mono<LayerEntity> getByIdAndUserId(String id, String userId) {
+        return layerRepository.findByIdAndUserId(id, userId);
+    }
+    @Override
+    public Mono<LayerEntity> addSupportToLayer(TutorEntity tutor, String supportId) {
+        return layerRepository.findById(tutor.getLayerId())
+                .map(layer -> {
+                    layer.getSupportIds().add(supportId);
+                    return layer;
+                })
+                .flatMap(layerRepository::save);
     }
 }
