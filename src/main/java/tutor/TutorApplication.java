12a878b0ee6474b9d305486cd80452c6e67b06d3
TNTTroll <60446797+TNTTroll@users.noreply.github.com>
2024-12-14 11:58:34 +0300

    Update TutorApplication.java

--- src/main/java/tutor/TutorApplication.java
+++ src/main/java/tutor/TutorApplication.java
@@ -3,6 +3,7 @@ package main.java.tutor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication(scanBasePackages = "tutor")
 public class TutorApplication {
     public static void main(String[] args) {
         SpringApplication.run(TutorApplication.class, args);
